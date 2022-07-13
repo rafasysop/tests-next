c882168f910343821856e7d8b7b639d8977cf596
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-12 22:08:49 -0300

    resolve filter products

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Search from '../components/search';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     axios.get('/api/products').then(({ data: { products } }) => {
@@ -12,15 +13,17 @@ export default function Home() {
     });
   }, []);
 
+  const doSearch = term => setFilter(term);
+
   return (
     <main className="my-8">
-      <Search />
+      <Search doSearch={doSearch} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">{products.length} Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {products
-            .filter(item => item.title.includes(filter))
+            .filter(item => item.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
             .map(product => (
               <ProductCard product={product} key={product.id} />
             ))}
